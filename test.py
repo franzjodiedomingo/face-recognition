1230b0449f6817d85a8fbe3ff2b2dfe26757257c
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-02-10 19:30:54 +0800

    frame bg

--- test.py
+++ test.py
@@ -3,16 +3,22 @@ import cv2
 import pickle
 import numpy as np
 import os
+import csv
+import time
+from datetime import datetime
+
 video=cv2.VideoCapture(0)
 facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
-with open('data/names.pkl', 'rb') as f:
-    LABELS=pickle.load(f)
+with open('data/names.pkl', 'rb') as w:
+    LABELS = pickle.load(w)
 with open('data/faces_data.pkl', 'rb') as f:
-    FACES=pickle.load(f)
+    FACES = pickle.load(f)
 
 knn=KNeighborsClassifier(n_neighbors=5)
 knn.fit(FACES, LABELS)
+
+imgBackground=cv2.imread("background.png")
 
 while True:
     ret,frame=video.read()
@@ -22,9 +28,15 @@ while True:
         crop_img=frame[y:y+h, x:x+w, :]
         resized_img=cv2.resize(crop_img, (50,50)).flatten().reshape(1,-1)
         output=knn.predict(resized_img)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),2)
+        cv2.rectangle(frame,(x,y-40),(x+w,y),(50,50,255),-1)
+        cv2.putText(frame, str(output[0]), (x,y-15), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 1)
         cv2.putText(frame, str(output[0]), (x,y-15), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (60,60,255), 1)
-    cv2.imshow("Frame", frame)
+    imgBackground[162:162 + 480, 55:55 + 640] = frame
+    cv2.imshow("Frame",imgBackground)
     k=cv2.waitKey(1)
     if k==ord('q'):
         break
