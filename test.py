2cd07d3ae4fc7e5ac31264e80926d18117ae4036
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-02-10 14:47:47 +0800

    adding pkl file

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 from sklearn.neighbors import KNeighborsClassifier
-
-
 import cv2
 import pickle
 import numpy as np
@@ -8,11 +6,13 @@ import os
 video=cv2.VideoCapture(0)
 facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
-faces_data=[]
+with open('data/names.pkl', 'rb') as f:
+    LABELS=pickle.load(f)
+with open('data/faces_data.pkl', 'rb') as f:
+    FACES=pickle.load(f)
 
-i=0
-
-name=input("Enter Your Name: ")
+knn=KNeighborsClassifier(n_neighbors=5)
+knn.fit(FACES, LABELS)
 
 while True:
     ret,frame=video.read()
@@ -20,39 +20,13 @@ while True:
     faces=facedetect.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         crop_img=frame[y:y+h, x:x+w, :]
-        resized_img=cv2.resize(crop_img, (50,50))
-        if len(faces_data)<=100 and i%10==0:
-            faces_data.append(resized_img)
-        i=i+1
-        cv2.putText(frame, str(len(faces_data)), (50,50), cv2.FONT_HERSHEY_TRIPLEX, 1, (50,50,255), 1)
+        resized_img=cv2.resize(crop_img, (50,50)).flatten().reshape(1,-1)
+        output=knn.predict(resized_img)
+        cv2.putText(frame, str(output[0]), (x,y-15), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (60,60,255), 1)
-    cv2.imshow("frame", frame)
+    cv2.imshow("Frame", frame)
     k=cv2.waitKey(1)
-    if k==ord('q') or len(faces_data)==100:
+    if k==ord('q'):
         break
 video.release()
 cv2.destroyAllWindows()
-
-faces_data=np.asarray(faces_data)
-faces_data=faces_data.reshape(100, -1)
-
-if 'names.pkl' not in os.listdir('data/'):
-    names=[name]*100
-    with open('data/names.pkl', 'wb') as f:
-        pickle.dump(names, f)
-else:
-    with open('data/names.pkl', 'rb') as f:
-        names=pickle.load(f)
-    names=names+[name]*100
-    with open('data/names.pkl', 'wb') as f:
-        pickle.dump(names, f)
-
-if 'faces_data.pkl' not in os.listdir('data/'):
-    with open('data/faces_data.pkl', 'wb') as f:
-        pickle.dump(faces_data, f)
-else:
-    with open('data/faces_data.pkl', 'rb') as f:
-        faces=pickle.load(f)
-    faces=np.append(faces, faces_data, axis=0)
-    with open('data/faces_data.pkl', 'wb') as f:
-        pickle.dump(faces, f)
