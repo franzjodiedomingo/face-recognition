c2efd2ffdb8c84e12c4032b04bdf8e47e426c48d
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-02-10 12:42:55 +0800

    test

--- add_faces.py
+++ add_faces.py
@@ -43,3 +43,13 @@ else:
     names=names+[name]*100
     with open('data/names.pkl', 'wb') as f:
         pickle.dump(names, f)
+
+if 'faces_data.pkl' not in os.listdir('data/'):
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces_data, f)
+else:
+    with open('data/faces_data.pkl', 'rb') as f:
+        faces=pickle.load(f)
+    faces=np.append(faces, faces_data, axis=0)
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces, f)
